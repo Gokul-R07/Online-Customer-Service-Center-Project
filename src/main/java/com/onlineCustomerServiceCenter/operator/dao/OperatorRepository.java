e1e3ed6cd313d2b931ddd2a089234f70fba826fd
Gokul <gokulramdevan@gmail.com>
2024-03-06 21:23:13 +0530

    test cases added

--- src/main/java/com/onlineCustomerServiceCenter/operator/dao/OperatorRepository.java
+++ src/main/java/com/onlineCustomerServiceCenter/operator/dao/OperatorRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface OperatorRespository extends JpaRepository<Operator,Integer> {
-    Optional<OperatorLoginDto> findOperatorByEmail(String email);
+    Optional<Operator> findOperatorByEmail(String email);
 }
