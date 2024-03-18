bc98b5d15f9e9f37d7579fdd3918992eec28e9e8
Gokul <gokulramdevan@gmail.com>
2024-03-18 11:02:34 +0530

    erorr resolved

--- src/main/java/com/onlineCustomerServiceCenter/admin/service/AdminServiceImpl.java
+++ src/main/java/com/onlineCustomerServiceCenter/admin/service/AdminServiceImpl.java
@@ -14,26 +14,26 @@ public class AdminServiceImpl implements AdminService {
 
     private final List<Admin> adminList = new ArrayList<>();
 
-    @Override
-    public Admin adminLogin(AdminLoginDto adminLoginDto) {
-        // Simulated logic to validate admin login
-        for (Admin admin : adminList) {
-            if (admin.getPassword().equals(adminLoginDto.getPassword()))
-                if (admin.getEmail().equals(adminLoginDto.getEmail())) {
-                    return admin; // Return the logged-in admin
-                }
-        }
-        return null; // Return null if login credentials are invalid
-    }
-
-    @Override
-    public Admin registerAdmin(AdminRegistrationDto adminRegistrationDto) {
-        // Simulated logic to register a new admin
-        Admin newAdmin = new Admin(adminRegistrationDto.getFirstName(), adminRegistrationDto.getLastName(),
-                adminRegistrationDto.getEmail(), adminRegistrationDto.getPassword());
-        adminList.add(newAdmin); // Add the new admin to the list
-        return newAdmin; // Return the registered admin
-    }
+//    @Override
+//    public Admin adminLogin(AdminLoginDto adminLoginDto) {
+//        // Simulated logic to validate admin login
+//        for (Admin admin : adminList) {
+//            if (admin.getPassword().equals(adminLoginDto.getPassword()))
+//                if (admin.getEmail().equals(adminLoginDto.getEmail())) {
+//                    return admin; // Return the logged-in admin
+//                }
+//        }
+//        return null; // Return null if login credentials are invalid
+//    }
+//
+//    @Override
+//    public Admin registerAdmin(AdminRegistrationDto adminRegistrationDto) {
+//        // Simulated logic to register a new admin
+//        Admin newAdmin = new Admin(adminRegistrationDto.getFirstName(), adminRegistrationDto.getLastName(),
+//                adminRegistrationDto.getEmail(), adminRegistrationDto.getPassword());
+//        adminList.add(newAdmin); // Add the new admin to the list
+//        return newAdmin; // Return the registered admin
+//    }
 
     @Override
     public boolean deleteOperator(int operatorId) {
